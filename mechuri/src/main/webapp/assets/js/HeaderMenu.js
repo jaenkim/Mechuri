3eb8d85efd22b6af2120e6698002640f9072e5e2
HKEDU <HKEDU@192.168.5.111>
2019-07-02 17:35:56 +0900

    메뉴 완성

--- mechuri/src/main/webapp/assets/js/HeaderMenu.js
+++ mechuri/src/main/webapp/assets/js/HeaderMenu.js
@@ -1,28 +1,11 @@
 /**
  * 
  */
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
-
-/*
-<button class="accordion">Section 1</button>
-<div class="panel">
-  <p>Lorem ipsum...</p>
-</div>*/
+$(function() {
+	$("#accordian p").click(function() {
+		$("#accordian ul ul").slideUp();
+		if (!$(this).next().is(":visible")) {
+			$(this).next().slideDown();
+		}
+	})
+})
