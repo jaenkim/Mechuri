b371df47a225191c24c49dff6e4613f9747c33f3
alfk0 <alfk0@DESKTOP-2581L12>
2019-07-07 15:34:40 +0900

    commit 미라

--- mechuri/src/main/java/com/hk/mechuri/AController.java
+++ mechuri/src/main/java/com/hk/mechuri/AController.java
@@ -21,21 +21,21 @@ import com.hk.mechuri.service.IMembersService;
 @Controller
 public class AController {
 	private static final Logger logger = LoggerFactory.getLogger(AController.class);
-	
+
 	@Autowired
 	private IMembersService membersService;
-	
+
 	@RequestMapping(value = "/signUp.do", method = {RequestMethod.GET, RequestMethod.POST})
 	public String signUp(Model model) {
 		logger.info("회원 추가폼으로 이동 {}.");
 		return "signUp"; 
-		}
+	}
 
 	@RequestMapping(value = "/signUpBoard.do", method = {RequestMethod.GET,RequestMethod.POST})
 	public String signUpBoard(Model model, membersDto dto) {
-					
+
 		logger.info("회원 추가합니다. {}.");
-		
+
 		boolean isS=membersService.signUpBoard(dto);
 		if(isS) {
 			return "redirect:main.do";
@@ -44,60 +44,74 @@ public class AController {
 			return "error";
 		}
 	}
-	
+
 	@RequestMapping(value = "/memLogin.do", method = RequestMethod.GET)
 	public String memLogin( Model model) {
-	
 		return "memLogin";
 	}
-	
-	@RequestMapping(value = "/compSignUp.do")
+
+
+	@RequestMapping(value = "/compSignUp.do", method = {RequestMethod.GET, RequestMethod.POST})
 	public String compSignUp(Model model) {
-	
+		logger.info("회원 추가폼으로 이동 {}.");
 		return "compSignUp";
 	}
-	
-	
+
+	@RequestMapping(value = "/compSignUpBoard.do", method = {RequestMethod.GET,RequestMethod.POST})
+	public String compSignUpBoard(Model model, membersDto dto) {
+
+		logger.info("회원 추가합니다. {}.");
+
+		boolean isS=membersService.compSignUpBoard(dto);
+		if(isS) {
+			return "redirect:main.do";
+		}else {
+			model.addAttribute("msg","회원가입 실패");
+			return "error";
+		}
+	}
+
+
 	@RequestMapping(value = "/groupbuying.do", method = RequestMethod.GET)
 	public String groupbuying(Model model) {
-	
+
 		return "groupbuying";
 	}
-	
+
 	@RequestMapping(value = "/groupbuyingContents.do", method = RequestMethod.GET)
 	public String groupbuyingContents(Model model) {
-	
+
 		return "groupbuyingContents";
 	}
 	@RequestMapping(value = "/groupbuyingContents2.do", method = RequestMethod.GET)
 	public String groupbuyingContents2(Model model) {
-	
+
 		return "groupbuyingContents2";
 	}
 	@RequestMapping(value = "/groupbuyingContents3.do", method = RequestMethod.GET)
 	public String groupbuyingContents3(Model model) {
-	
+
 		return "groupbuyingContents3";
 	}
-	
+
 	@RequestMapping(value = "/groupbuyingContents4.do", method = RequestMethod.GET)
 	public String groupbuyingContents4(Model model) {
-	
+
 		return "groupbuyingContents4";
 	}
 	@RequestMapping(value = "/groupbuyingContents5.do", method = RequestMethod.GET)
 	public String groupbuyingContents5(Model model) {
-	
+
 		return "groupbuyingContents5";
 	}
 	@RequestMapping(value = "/groupbuyingContents6.do", method = RequestMethod.GET)
 	public String groupbuyingContents6(Model model) {
-	
+
 		return "groupbuyingContents6";
 	}
 	@RequestMapping(value = "/groupbuyingContents7.do", method = RequestMethod.GET)
 	public String groupbuyingContents7(Model model) {
-	
+
 		return "groupbuyingContents7";
 	}
 	@RequestMapping(value = "/groupbuyingContents8.do", method = RequestMethod.GET)
