82602c4875b6f9b981681230007046ae3ed086a2
alfk0 <alfk0@DESKTOP-2581L12>
2019-07-12 07:00:59 +0900

    아침커밋

--- mechuri/src/main/java/com/hk/mechuri/MailSendingController.java
+++ mechuri/src/main/java/com/hk/mechuri/MailSendingController.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
  
 @Controller
-public class MailController {
+public class MailSendingController {
  
   @Autowired
   private JavaMailSender mailSender;
