f5a52e5f631f0894e7b8cf47bcfd4d73c933a712
alfk0 <alfk0@192.168.5.134>
2019-07-12 16:43:01 +0900

    commit

--- mechuri/src/main/java/com/hk/mechuri/naver/LoginController.java
+++ mechuri/src/main/java/com/hk/mechuri/naver/LoginController.java
@@ -29,7 +29,7 @@ public class LoginController {
 
     //로그인 첫 화면 요청 메소드
     @RequestMapping(value = "/naverLogin.do", method = { RequestMethod.GET, RequestMethod.POST })
-    public String login(Model model, HttpSession session) {
+    public String naverLogin(Model model, HttpSession session) {
         
         /* 네이버아이디로 인증 URL을 생성하기 위하여 naverLoginBO클래스의 getAuthorizationUrl메소드 호출 */
         String naverAuthUrl = naverLoginBO.getAuthorizationUrl(session);
